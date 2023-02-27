f13f7a3e1304691cabebb1f58825256409177d87
ElenaMegia <emegican@myuax.com>
2023-02-27 15:54:10 +0100

    Initial commit

---
+++ src/main/java/com/example/ejerciciostema7/EjerciciosTema7Application.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciostema7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosTema7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjerciciosTema7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejerciciostema7/EjerciciosTema7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciostema7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosTema7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
